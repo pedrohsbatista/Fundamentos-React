8d5d31cc9dd33b6e64a5ad5b5052d59cea3d1d5a
Pedro Batista <pedrohenrique.sb07@gmail.com>
2021-11-30 20:57:17 -0300

    Projeto inicial

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+const tag = <strong>Olá React!</strong>
+
+ReactDOM.render(
+    <div>
+       { tag }
+    </div>,
+    document.getElementById('root')
+)
