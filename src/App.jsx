982f3029fd4c6cc548eba51555e42e2dbdbaa561
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-02-05 10:13:19 -0300

    Fundamentos declaração de function

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
+
+//Usar default pode usar função anônima (sem atribuir um nome)
+
+// function ou arrow function(=>) mesma sintaxe
+//export default function(props){
+
+//Arrow function parâmetrização:
+// se tiver 1 parâmetro pode remover o parênteses
+// se não tiver parâmetro pode usar parênteses vazio
+// se tiver 1 parâmetro não utilizado pode usar _
+
+//Usar arrow function pode remover o corpo do componente
+// {
+//   return (
+//   )
+// }
+
+export default _ =>
+    <div id="app">
+        <h1>Fundamentos React</h1>
+        <Fragmento />
+        <ComParametro
+            titulo="Situação do Aluno"
+            aluno="Pedro Silva"
+            nota={9.3} />
+        <Primeiro></Primeiro>
+    </div>
--- src/index.js
+++ src/index.js
@@ -2,23 +2,14 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import './index.css'
 
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
-import Fragmento from './components/basicos/Fragmento'
+import App from './App'
 
 //const tag = <strong>Olá React!</strong>
 
 ReactDOM.render(
     //<div>
     //   { tag }
-    //</div>, 
-    <div id="app">
-        <Primeiro></Primeiro>  
-        <ComParametro 
-         titulo="Situação do Aluno" 
-         aluno="Pedro Silva"
-         nota={9.3} />                              
-         <Fragmento />
-    </div>,
+    //</div>,    
+    <App />,
     document.getElementById('root')
 )
